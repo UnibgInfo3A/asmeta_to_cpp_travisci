051e7f20e1f44ab28b96f01a61a05e49bb31970c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-25 22:20:50 +0000

    Deploy UnibgInfo3A/asmeta_to_cpp_travisci to github.com/UnibgInfo3A/asmeta_to_cpp_travisci.git:gh-pages

--- test/asmeta_to_cpp_travisci/ParserTest.java
+++ test/asmeta_to_cpp_travisci/ParserTest.java
@@ -5,7 +5,10 @@ package asmeta_to_cpp_travisci;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 import org.asmeta.parser.ASMParser;
 import org.junit.Test;
@@ -15,9 +18,14 @@ public class ParserTest {
 	@Test
 	public void test() {
 		try {
-			File file = new File("asm_examples/ferrymanSimulator.asm");
+			String asmFile = "asm_examples/ferrymanSimulator.asm";
+			File file = new File(asmFile);
 			assertTrue(file.exists());
 			ASMParser.setUpReadAsm(file);
+			// write to page file the results of the parser
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("docs/results.txt", true)));
+			out.println(asmFile);
+			out.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
