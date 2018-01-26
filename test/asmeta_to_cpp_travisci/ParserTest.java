9c56a8c459f10ae8b70af7ee64f6e80f63e26267
silviabonfanti <silvia.bonfanti@unibg.it>
2018-01-26 17:00:05 +0100

    update

--- test/asmeta_to_cpp_travisci/ParserTest.java
+++ test/asmeta_to_cpp_travisci/ParserTest.java
@@ -1,5 +1,5 @@
 package asmeta_to_cpp_travisci;
-
+ 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -12,8 +12,8 @@ import java.sql.Timestamp;
 import org.asmeta.parser.ASMParser;
 import org.junit.Test;
 
-public class ParserTest {
-
+public class ParserTest { 
+ 
 	private static final String RESULTS_INDEX_HTML = "docs/index.html";
 
 	@Test
