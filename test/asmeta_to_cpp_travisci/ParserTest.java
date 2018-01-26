3b580bd3773cbfd75a0482fafc9736b2f1433337
Angelo Gargantini <angelo.gargantini@unibg.it>
2018-01-26 10:38:08 +0100

    to docs

--- test/asmeta_to_cpp_travisci/ParserTest.java
+++ test/asmeta_to_cpp_travisci/ParserTest.java
@@ -14,44 +14,33 @@ import org.junit.Test;
 
 public class ParserTest {
 
+	private static final String RESULTS_INDEX_HTML = "docs/index.html";
+
 	@Test
 	public void test() {
-		try {
-			String asmFile = "asm_examples/ferrymanSimulator.asm";
-			File file = new File(asmFile);
-			assertTrue(file.exists());
-			ASMParser.setUpReadAsm(file);
-			// write to page file the results of the parser
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("results/index.html", true)));
-			// add time stamp
-			out.print(new Timestamp(System.currentTimeMillis()) + "\t");
-			// name of the file
-			out.println(asmFile);
-			out.close();
-			System.out.println(new File("results/index.html").getAbsolutePath());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail("");
-		}
-	}
-	
+		String asmFile = "asm_examples/ferrymanSimulator.asm";
+		testAsmFile(asmFile);
+	}	
 
 	@Test
 	public void test2() {
+		String asmFile = "asm_examples/coffeeVendingMachine.asm";
+		testAsmFile(asmFile);
+	}
+
+	private void testAsmFile(String asmFile) {
 		try {
-			String asmFile = "asm_examples/coffeeVendingMachine.asm";
 			File file = new File(asmFile);
 			assertTrue(file.exists());
 			ASMParser.setUpReadAsm(file);
 			// write to page file the results of the parser
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("results/index.html", true)));
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(RESULTS_INDEX_HTML, true)));
 			// add time stamp
 			out.print(new Timestamp(System.currentTimeMillis()) + "\t");
 			// name of the file
 			out.println(asmFile);
 			out.close();
-			System.out.println(new File("results/index.html").getAbsolutePath());
+			System.out.println(new File(RESULTS_INDEX_HTML).getAbsolutePath());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
