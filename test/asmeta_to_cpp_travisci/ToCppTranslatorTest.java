3ca44c972d7c0c358994e2c4f150cab404a51473
Angelo Gargantini <angelo.gargantini@unibg.it>
2018-01-31 19:12:59 +0100

    [ci skip] aggiunto test per generatore

---
+++ test/asmeta_to_cpp_travisci/ToCppTranslatorTest.java
@@ -0,0 +1,35 @@
+package asmeta_to_cpp_travisci;
+ 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+
+import org.asmeta.asm2code.GeneratorCompilerTest;
+import org.asmeta.parser.ASMParser;
+import org.junit.Test;
+
+public class ToCppTranslatorTest { 
+ 
+
+	@Test
+	public void test() {
+		String asmFile = "asm_examples/ferrymanSimulator.asm";
+		testAsmFile(asmFile);
+	}	
+	private void testAsmFile(String asmFile) {
+		try {
+			GeneratorCompilerTest.test(asmFile);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("");
+		}
+	}
+	
+
+}
