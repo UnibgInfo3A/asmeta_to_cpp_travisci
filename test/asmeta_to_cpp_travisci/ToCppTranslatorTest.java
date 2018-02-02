e6989d480c082438da7a09a870edd716664bffc7
silviabonfanti <silvia.bonfanti@unibg.it>
2018-02-02 09:34:36 +0100

    update

--- test/asmeta_to_cpp_travisci/ToCppTranslatorTest.java
+++ test/asmeta_to_cpp_travisci/ToCppTranslatorTest.java
@@ -2,6 +2,11 @@ package asmeta_to_cpp_travisci;
 
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.asmeta.asm2code.GeneratorCompilerTest;
 import org.junit.Test;
 
@@ -9,11 +14,30 @@ public class ToCppTranslatorTest {
 
 
 	@Test
-	public void test() {
-		String asmFile = "asm_examples/coffeeVendingMachineNC.asm";
-		testAsmFile(asmFile);
-	}	
+	public void testAllFiles() {
+		for (String asmFile : getAllASMFiles()) {
+			testAsmFile(asmFile);
+		}
+	}
 	
+	
+	List<String> getAllASMFiles() {
+		return getAllASMFiles(new File("asm_examples"));
+	}
+	
+	List<String> getAllASMFiles(File dir) {
+		assert Files.isDirectory(dir.toPath());
+		List<String> result = new ArrayList<>();
+		File[] listOfFiles = dir.listFiles();
+		for (File file : listOfFiles) {
+			if (file.isFile() && file.getName().endsWith(".asm")) {
+				result.add(file.getPath());
+			} else if (file.isDirectory()) {
+				result.addAll(getAllASMFiles(file));
+			}
+		}
+		return result;
+	}
 	
 	private void testAsmFile(String asmFile) {
 		try {
