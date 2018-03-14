2d2b9c3561353e1ebaa186375281b90270f6b671
silviabonfanti <silvia.bonfanti@unibg.it>
2018-03-14 17:29:26 +0100

    update

--- examples_testOK/iterativeWhile.asm
+++ examples_testOK/iterativeWhile.asm
@@ -4,17 +4,22 @@ import StandardLibrary
 
 signature:
 	controlled contrC: Integer
-	monitored mon: Integer
+	controlled controlledF: Integer
 	monitored doWhile: Boolean
 
 definitions:
 	main rule r_Main =
 		if(doWhile) then 
-			while(contrC < mon) do
+			while(contrC < controlledF) do
 				contrC := contrC + 1
 		else
+			par
 			contrC := 0
+			controlledF:=5
+			endpar
 		endif
 
 default init s0:
 	function contrC = 3
+	function doWhile = true
+	function controlledF=5
