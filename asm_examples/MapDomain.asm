b390f7abae75c43e1fb4acd8bd37b3fd52a07a2d
silviabonfanti <silvia.bonfanti@unibg.it>
2018-04-19 17:54:11 +0200

    update spec

---
+++ asm_examples/MapDomain.asm
@@ -0,0 +1,21 @@
+/** at every step increments the seconds 
+*/
+asm MapDomain
+
+import StandardLibrary
+
+signature:
+	controlled voti: Map(String, Integer)
+
+
+definitions:
+
+
+	main rule r_Main = 
+		voti := {"A"-> 5}
+
+
+
+default init s0:
+
+
